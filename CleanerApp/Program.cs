61cff71594f1fd9d4edbd7332219118474ec647a
Dan M <the.code.writer@gmail.com>
2015-02-13 18:01:08 -0700

    Very basic project start.  Takes filepath input and passes it to the cleaner library.  No actual functionality implemented.

---
+++ CleanerApp/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ReadcleanCleaner;
+
+namespace CleanerApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                printUsage();
+                return;
+            }
+
+            string filepath = args[0];
+
+            Cleaner cleaner = new Cleaner(filepath, filepath + "_clean"); // TODO handle file extension better
+            cleaner.Clean();
+
+            Console.ReadLine();
+        }
+
+        public static void printUsage()
+        {
+            // TODO
+        }
+    }
+}
---
+++ Source/Cleaner.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReadcleanCleaner
+{
+    public class Cleaner
+    {
+        public Cleaner(string originalFilepath, string destinationFilepath)
+        {
+            // TODO
+        }
+
+        public void Clean()
+        {
+            // TODO
+        }
+    }
+}
